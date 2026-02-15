bac15e1ccd28ff74be7b51421e5f3af9b8b1c396
Modini Padma Sree <modinisree@gmail.com>
2026-02-15 18:51:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CD Pipeline version2 Working Successfully!"
+    return "CD Pipeline Working Successfully!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
